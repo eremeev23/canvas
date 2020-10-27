988bc2f23a00d22d649e096b4f2f9b7b8c1d2563
Maksim Eremeev <eremeevmiami94@icloud.com>
2020-10-27 19:31:05 +0300

    /

---
+++ script.js
@@ -0,0 +1,27 @@
+let canvas = document.getElementById('tutorial');
+let context = canvas.getContext('2d');
+    //net
+let x = document.getElementById("inp-x").value;
+let y = document.getElementById("inp-y").value;
+
+function make(){
+    for (let i = 40; i < x; i += 20) {
+    context.moveTo(i, 20);
+    context.lineTo(i, y);
+    }
+    for (let k = 40; k < y; k += 20) {
+    context.moveTo(20, k);
+    context.lineTo(x, k);
+    }
+    context.strokeStyle = "#151033";
+    context.stroke();
+}
+make();
+    //figures
+let fgr = canvas.getContext('2d');
+fgr.fillStyle = "#241b55";
+fgr.fillRect (300, 10, 20, 20);
+fgr.fillRect (335, 10, 40, 40);
+fgr.fillRect (390, 10, 40, 20);
+fgr.fillRect (300, 60, 60, 80);
+    //moving
