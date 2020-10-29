b1fbf47045ae42d9c83e3010dae69fcd86205e32
Maksim Eremeev <63352937+maxon32@users.noreply.github.com>
2020-10-29 18:35:27 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 let canvas = document.getElementById('tutorial');
 let context = canvas.getContext('2d');
     //net
-let x = document.getElementById("inp-x").value;
-let y = document.getElementById("inp-y").value;
-
 function make(){
-    for (let i = 40; i < x; i += 20) {
+    context.beginPath();
+    let x = document.getElementById("inp-x").value,
+        y = document.getElementById("inp-y").value;
+    for (let i = 20; i <= x; i += 20) {
     context.moveTo(i, 20);
     context.lineTo(i, y);
-    }
-    for (let k = 40; k < y; k += 20) {
+    };
+    for (let k = 20; k <= y; k += 20) {
     context.moveTo(20, k);
     context.lineTo(x, k);
-    }
+    };
     context.strokeStyle = "#151033";
     context.stroke();
 }
@@ -20,7 +20,7 @@ make();
     //figures
 let fgr = canvas.getContext('2d');
 fgr.fillStyle = "#241b55";
-fgr.fillRect (300, 10, 20, 20);
-fgr.fillRect (335, 10, 40, 40);
-fgr.fillRect (390, 10, 40, 20);
-fgr.fillRect (300, 60, 60, 80);
+fgr.fillRect (400, 10, 20, 20);
+fgr.fillRect (435, 10, 40, 40);
+fgr.fillRect (490, 10, 40, 20);
+fgr.fillRect (400, 60, 60, 80);
