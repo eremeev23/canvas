f51a7db6da0b8b9c6118a5b6f5097b43a6d57c24
Maksim Eremeev <eremeevmiami94@icloud.com>
2021-03-27 16:38:27 +0300

    + moving

--- script.js
+++ script.js
@@ -1,91 +1,111 @@
+var mouse = {
+    x: 0,
+    y: 0,
+  };
+var selected = false;       
+
 const submitButton = document.querySelector('.btn');
+const deleteButton = document.querySelector('.delete');
 const inputX = document.querySelector('.form-control-x');
 const inputY = document.querySelector('.form-control-y');
 
-let canvas = document.getElementById('tutorial');
-let context = canvas.getContext('2d');
+const cnv = document.querySelector('.canvas');
+const ctx = cnv.getContext('2d');
     
     //net
-    submitButton.addEventListener('click', make);
-    function make(event){
+    submitButton.addEventListener('click', createNet);
+    function createNet(event){
     event.preventDefault();
 
-    context.beginPath();
-
+    ctx.beginPath();
+    
     let x = inputX.value,
         y = inputY.value;
-
     for (let i = 20; i <= x; i += 20) {
-    context.moveTo(i, 20);
-    context.lineTo(i, y);
+    ctx.moveTo(i, 20);
+    ctx.lineTo(i, y);
     };
 
     for (let k = 20; k <= y; k += 20) {
-    context.moveTo(20, k);
-    context.lineTo(x, k);
+    ctx.moveTo(20, k);
+    ctx.lineTo(x, k);
     };
 
-    context.strokeStyle = "#000000";
-    context.stroke();
-
+    ctx.lineWidth = 2;
+    ctx.strokeStyle = "#985DTD";
+    ctx.stroke();
+    
     inputX.value = "";
     inputY.value = "";
 };
 
+    //clear canvas
+deleteButton.addEventListener('click', clearCanvas);
+function clearCanvas(event){
+  ctx.clearRect(0, 0, 600, 600);
+};
+
     //figures
-let fgrOne = canvas.getContext('2d');
-fgrOne.fillStyle = "#000000";
-fgrOne.fillRect (700, 10, 20, 20);
-
-let fgrTwo = canvas.getContext('2d');
-fgrTwo.fillStyle = "#000000";
-fgrTwo.fillRect (735, 10, 40, 40);
-
-let fgrThree = canvas.getContext('2d');
-fgrThree.fillStyle = "#000000";
-fgrThree.fillRect (790, 10, 40, 20);
-
-let fgrFour = canvas.getContext('2d');
-fgrFour.fillStyle = "#000000";
-fgrFour.fillRect (700, 60, 60, 80);
-    //moving
-
-// fgrOne.onmousedown = function(e) {
-
-//     let coords = getCoords(fgrOne);
-//     let shiftX = e.pageX - coords.left;
-//     let shiftY = e.pageY - coords.top;
-      
-//     fgrOne.style.position = 'absolute';
-//     document.body.appendChild(fgrOne);
-//     moveAt(e);
-      
-//     fgrOne.style.zIndex = 1000; // над другими элементами
-      
-//     function moveAt(e) {
-//         fgrOne.style.left = e.pageX - shiftX + 'px';
-//         fgrOne.style.top = e.pageY - shiftY + 'px';
-//     }
-      
-//     document.onmousemove = function(e) {
-//         moveAt(e);
-//     };
-      
-//     fgrOne.onmouseup = function() {
-//         document.onmousemove = null;
-//         fgrOne.onmouseup = null;
-//     };
-      
-//     }
-      
-//     fgrOne.ondragstart = function() {
-//     return false;
-// };
-      
-// function getCoords(elem) {   // кроме IE8-
-//     var box = elem.getBoundingClientRect();
-//     return {
-//         top: box.top + pageYOffset,
-//         left: box.left + pageXOffset
-//     };
-// }
+var Rect = function(...points) {
+    this.points = points;
+};
+Rect.prototype = {
+    draw() {
+    ctx.fillRect(this.points[0].x, this.points[0].y, this.points[1].x - this.points[0].x, this.points[1].y - this.points[0].y)
+    },
+    mouseIn() {
+    return (
+        mouse.x >= this.points[0].x && mouse.x <= this.points[1].x &&
+        mouse.y >= this.points[0].y && mouse.y <= this.points[1].y
+          )
+        }
+};
+var shapes = [
+    new Rect(
+      { x: 600, y: 100 },
+      { x: 700, y: 200 }
+    ),
+    new Rect(
+      { x: 500, y: 350 },
+      { x: 550, y: 450 }
+    ),
+    new Rect(
+      { x: 500, y: 200 },
+      { x: 570, y: 300 }
+    ),
+    new Rect(
+        {x: 700, y: 10},
+        {x: 750, y: 15}
+    )    
+  ];
+  
+      //moving
+shapes.forEach(n => n.draw());
+  
+window.onmousemove = function(e) {
+  mouse.x = e.pageX - cnv.offsetLeft;
+  mouse.y = e.pageY - cnv.offsetTop;
+  
+  if (selected) {
+    selected.points.forEach(n => {
+      n.x += e.movementX;
+      n.y += e.movementY;
+    });
+    ctx.clearRect(0, 0, cnv.width, cnv.height);
+    shapes.forEach(n => n.draw());
+  }
+};
+          
+ window.onmousedown = function() {
+  if (!selected) {
+    shapes.forEach(n => {
+      if (n.mouseIn(mouse)) {
+        selected = n;
+      }
+    })
+  };
+};
+  
+window.onmouseup = function() {
+  selected = null;
+};
